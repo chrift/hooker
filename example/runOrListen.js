6d64daabdd2bcfcbe2a0eb0e4de5207fb7aa9a3f
chrischeshire <2837753+chrift@users.noreply.github.com>
2020-09-30 14:25:34 +0100

    Rename to githooker Examples

---
+++ example/runOrListen.js
@@ -0,0 +1,43 @@
+// Run with `node runOrListen.js run
+
+import hooker from 'hooker'
+import * as path from 'path'
+
+const credentials = {
+  'accessKeyId': 'XX',
+  'secretAccessKey': 'XX',
+  'region': 'eu-west-1',
+  'bucket': 'XX'
+}
+
+const slackWebHookUrl = 'https://hooks.slack.com/services/XX'
+
+const hookerConfig = {
+  name: 'Webhook name',
+  webhookSecret: 'XX',
+  slackWebHookUrl,
+  steps: [
+    hooker.job.slackMessage('Starting deployment...', slackWebHookUrl),
+    {
+      name: 'Run API release script',
+      action: hooker.job.executeFile(path.resolve(__dirname, '../../../another-project/api/release.sh'))
+    },
+    hooker.job.slackMessage('API released. Building Web App...', slackWebHookUrl),
+    {
+      name: 'Build React Web App',
+      action: hooker.job.buildReact(path.resolve(__dirname, '../../../another-project/webapp'))
+    },
+    hooker.job.slackMessage('Web App built successfully. Uploading Web App to S3...', slackWebHookUrl),
+    {
+      name: 'Upload Web App to S3',
+      action: hooker.job.uploadDirToS3(path.resolve(__dirname, '../../../another-project/webapp/build'), credentials)
+    },
+    hooker.job.slackMessage('Deployment completed successfully', slackWebHookUrl)
+  ]
+}
+
+if (process.argv[2] === 'run') {
+  hooker.runHookerConfig(hookerOptions)
+} else {
+  hooker.init(hookerOptions)
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Hooker - A Github webhook handler
+
+## To listen for webhook invocations
+[Example](example/listen.js)
+
+## To run manually
+[Example](example/run.js)
+
+## To listen or run manually
+[Example](example/runOrListen.js)
